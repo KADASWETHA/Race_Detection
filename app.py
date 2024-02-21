e1f77b44b9299dda8622ce41c15d40ef634749fd
KADA SWETHA <143429069+KADASWETHA@users.noreply.github.com>
2024-02-21 14:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,33 @@ import streamlit as st
 from deepface import DeepFace
 import cv2
 import numpy as np
+glowing_text_style = '''
+    <style>
+        .glowing-text {
+            font-family: 'Arial Black', sans-serif;
+            font-size: 33px;
+            text-align: center;
+            animation: glowing 2s infinite;
+        }
+        
+        @keyframes glowing {
+            0% { color: #FF9933; } /* Saffron color */
+            10% { color: #FFD700; } /* Gold color */
+            20% { color: #FF1493; } /* Deep Pink */
+            30% { color: #00FF00; } /* Lime Green */
+            40% { color: #FF4500; } /* Orange Red */
+            50% { color: #9400D3; } /* Dark Violet */
+            60% { color: #00BFFF; } /* Deep Sky Blue */
+            70% { color: #FF69B4; } /* Hot Pink */
+            80% { color: #ADFF2F; } /* Green Yellow */
+            90% { color: #1E90FF; } /* Dodger Blue */
+            100% { color: #FF9933; } /* Saffron color */
+        }
+    </style>
+'''
+
+
+st.markdown(glowing_text_style, unsafe_allow_html=True)
 st.markdown(f'''<p class="glowing-text">
 <h1 align='center'>Race Detection System</h1>''',unsafe_allow_html=True)
 
