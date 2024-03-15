cc3a736ea78f429fd3397ca220b188176ae4d6d7
KADA SWETHA <143429069+KADASWETHA@users.noreply.github.com>
2024-03-15 11:24:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,16 +34,16 @@ st.markdown(f'<p class="glowing-text"><h1 align="center">Race Detection System</
 
 cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-user_image=st.file_uploader('Please upload your image',type=['jpg','jpeg','png','tiff','psd','pdf','eps','indd','ai','avif'])
+user_image=st.file_uploader('Please upload your image',type=['jpg','jpeg','png','pdf'])
 btn=st.button('Predict')
 if btn and user_image is not None:
 	bytes_data = user_image.getvalue()
 	image= cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 	result=DeepFace.analyze(image,actions=['race'])
 	gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	faces = cascade.detectMultiScale(gray_frame, 1.1, 3)
+	faces = cascade.detectMultiScale(gray_frame, 1.6, 4)
 	for x,y,w,h in faces:
-		cv2.rectangle(image, (x, y), (x+w, y+h), (4, 29, 255), 2, cv2.LINE_4)
+		cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2, cv2.LINE_4)
 		user_selected_items = list(result[0].keys())
 		if 'dominant_race' in user_selected_items:
 			emotion_label='Race: '+str(result[0]['dominant_race']).title()
